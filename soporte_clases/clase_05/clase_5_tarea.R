7fb24adb984861bd432d24338c8d92d13cd027a4
javendaXgh <javenda@gmail.com>
2024-10-30 12:35:11 -0400

    clase 5 complemento

--- soporte_clases/clase_05/clase_5_tarea.R
+++ soporte_clases/clase_05/clase_5_tarea.R
@@ -1,26 +1,34 @@
 ##########################################################################################
 # Tareas clase 5 del 30-10-24
+#
 # 1) Instalar paquetes señalados en el repo 
-# según fecha 30-10 en e
+# https://github.com/UCVeconomia2024-2/scripts/blob/main/paquetes.R
+# según fecha 30-10
+
+# 2) Lectura y ejecución de chunks de guía asociada a la clase 5
+# https://github.com/UCVeconomia2024-2/scripts/blob/main/soporte_clases/clase_05/clase_5_complemento.qmd
+
+# 2) Ver video https://www.youtube.com/watch?v=WCDCOF47O_I&list=PLliutFhUtupKmtQ-P83-leQJOilGOEV60&index=2
+# y prestar atención y mucho entusiasmo a la exposición del Prof. Walter Sosa Escudero
+
+# 3) Inspeccionar Cheat Sheets de rmarkdown y rbase (revisar canal Discord 
+# "recursos-dispnibles)
+# La idea de las viñetas es contar con material resumido que nos permita conocer elementos
+# importantes sobre un paquete. No es para memorizarlas, sino para que cuando se tenga
+# una duda sobre el correspondiente paquete, se pueda acudir a la viñeta para encontrar 
+# referencias de manera rápida.
+
+# 4) Inspeccionar Vignette de ggplot2 "Introduction to ggplot2". Al ejecutar en la Consola
+# `??ggplot2` en la sección inferior derecha de ayuda de RStudio, aparece el acceso a la 
+# viñeta. Dar lectura y replicar en este script los códigos que en ella se muestran. 
+# Añadir comentarios si lo estima conveniente. Remitir script ejecutado. Luego se indicará
+# el método para remitir script
+
+
+# 5) Acceder al Cran (ver canal "recursos-disponibles" en Discord) y buscar en la sección 
+# Packages el paquete ggplot2. Dentro de la documentación del paquete, localizar la viñeta 
+# de nombre "Aesthetic specifications" y hacer lectura. No es necesario replicar los 
+#códigos
+
+# La parte 1 debe hacerse antes de la clase del 4-11-24. El resto debe ser antes del 11-11
 ##########################################################################################
-
-# vignettes
-# cheat sheets
-# paquetes instalar
-
-
-# verificar que no están instalados
-learnr
-shiny
-shinydashboard
-shinyWidgets
-htmlwidgets
-rmarkdown
-blogdown
-packrat
-rsconnect
-leaflet
-leaflet.extras
-
-lectura
-https://github.com/UCVeconomia2024-2/scripts/blob/main/soporte_clases/clase_03/clase_03_apoyo.qmd
