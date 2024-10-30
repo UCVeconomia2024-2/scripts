5df40dae7078d9762cda62421172a5ace24fed0b
javendaXgh <javenda@gmail.com>
2024-10-30 10:38:26 -0400

    clase 05

---
+++ paquetes.R
@@ -0,0 +1,30 @@
+##########################################################################################
+# Paquetes a instalar
+# listado de paquetes que se debe instalar a lo largo del curso
+#1) Previo a la instalación se debe verificar que no estén instalados intentando cargarlos
+#2) En caso de no estar instalado proceder con install.packages("nombre_paquete")
+#3) Posterior a que culmine el proceso de descarga e instalación del paquete proceder a
+# cargarlo para verificar que se instaló correctamente con library(nombre_paquete)
+##########################################################################################
+#29-10
+tidyverse
+plotly
+DT
+knitr
+rvest
+bookdown
+tinytex
+
+#30-10
+learnr
+shiny
+shinydashboard
+shinyWidgets
+htmlwidgets
+rmarkdown
+blogdown
+packrat
+rsconnect
+leaflet
+leaflet.extras
+tinytex::install_tinytex()
---
+++ soporte_clases/clase_05/clase_5_tarea.R
@@ -0,0 +1,26 @@
+##########################################################################################
+# Tareas clase 5 del 30-10-24
+# 1) Instalar paquetes señalados en el repo 
+# según fecha 30-10 en e
+##########################################################################################
+
+# vignettes
+# cheat sheets
+# paquetes instalar
+
+
+# verificar que no están instalados
+learnr
+shiny
+shinydashboard
+shinyWidgets
+htmlwidgets
+rmarkdown
+blogdown
+packrat
+rsconnect
+leaflet
+leaflet.extras
+
+lectura
+https://github.com/UCVeconomia2024-2/scripts/blob/main/soporte_clases/clase_03/clase_03_apoyo.qmd
